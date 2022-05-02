08410db1c8cafa4e5387ca73129a8aa80877f47b
alfa33333 <44150484+alfa33333@users.noreply.github.com>
2022-05-01 20:23:05 -0400

    added text reading
    Added function to read text and also display it. window bounding is still needed.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cerrno>
 #include "raylib.h"
 
+//String reading
+
+std::string get_file_contents(const char *filename)
+{
+  std::ifstream in(filename, std::ios::in | std::ios::binary);
+  if (in)
+  {
+    std::string contents;
+    in.seekg(0, std::ios::end);
+    contents.resize(in.tellg());
+    in.seekg(0, std::ios::beg);
+    in.read(&contents[0], contents.size());
+    in.close();
+    return(contents);
+  }
+  throw(errno);
+}
 
 int main(void)
 {
@@ -10,14 +31,34 @@ int main(void)
 
     InitWindow(screenWidth, screenHeight, "Basic window");
 
+
+    //message-setting
+    std::ifstream myfile("file.txt");
+    std::string line;
+
+
     //--------- text show example example
-    const char message[128] = "This sample illustrates a text writing\nanimation effect! Check it out! ;)";
 
     int framesCounter = 0;
 
     // //
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     // //--------------------------------------------------------------------------------------
+
+    //Get the lines from the file
+
+    if (myfile.is_open())
+    {
+        while ( getline (myfile,line) )
+        {
+            //   cout << line << '\n';
+            continue;
+        }
+        myfile.close();
+    } else {
+        std::cout << "Unable to open file";
+    } 
+
 
     // // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
@@ -39,7 +80,7 @@ int main(void)
 
             ClearBackground(BLACK);
             DrawFPS(0, 20);
-            DrawText(TextSubtext(message, 0, framesCounter/2), 210, 160, 20, GREEN);
+            DrawText(TextSubtext(line.c_str(), 0, framesCounter/2), 210, 160, 20, GREEN);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
